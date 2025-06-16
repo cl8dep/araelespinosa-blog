af33e8fa9685c1c71c936f00cffef85567aa073c
Arael Espinosa <cl8dep@gmail.com>
2025-06-16 00:19:31 -0300

    feat: deploy to Github Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
   organizationName: 'cl8dep', // Usually your GitHub org/user name.
   projectName: 'araelespinosa-blog', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
